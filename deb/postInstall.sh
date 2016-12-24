d2a1b76871e08566cde65275d69184494b88714a
Zile Rehman <zile@nyansa.com>
2016-12-23 16:43:17 -0800

    updates.

--- deb/postInstall.sh
+++ deb/postInstall.sh
@@ -6,6 +6,12 @@ echo "Extracting tar ball"
 cd ${package_dir}
 sudo tar xvf *.tar
 
-echo "Installing gems"
-gem install elasticsearch-model elasticsearch-rails
+echo "Installing Elastic Model"
+cd ${package_dir}/elasticsearch-model
+bundle install
+rake install
 
+echo "Installing Elastic Search Rails"
+cd ${package_dir}/elasticsearch-rails
+bundle install
+rake install
