961a3b7fcc4829fc2e0f700c35c2a8f60b421630
Zile Rehman <zile@nyansa.com>
2016-12-23 16:37:30 -0800

    updates.

--- deb/postInstall.sh
+++ deb/postInstall.sh
@@ -1,7 +1,6 @@
 service_name="voyance-elasticsearchrails"
 nyansa_dir=/opt/voyance
 package_dir=${nyansa_dir}/${service_name}
-supervisor_dir=/etc/supervisor/conf.d
 
 # Extract tar ball
 cd ${package_dir}
